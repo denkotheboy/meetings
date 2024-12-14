63be05e06cbcd0d35f83415b7f7582ccf067bef4
Kondratev.Denis <kondratev_dv@tochka.com>
2024-12-15 04:08:16 +0500

    feat(server): поправил подключение

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,35 +1,17 @@
-import { readFileSync } from "node:fs";
-import path from "node:path";
 import Websocket from "ws";
-import { createServer } from "https";
 
-const server = createServer({
-  cert: readFileSync(path.resolve(__dirname, "./cert.pem")),
-  key: readFileSync(path.resolve(__dirname, "./key.pem")),
-});
-const wsServer = new Websocket.Server({ server });
-
-const port = process.env.PORT || 9000;
-server.listen(port);
+const port = Number(process.env.PORT || 9000);
+const wsServer = new Websocket.Server({ port });
 
 console.log(`server started: ${port}`);
 
-let usersCount = 0;
-
 wsServer.addListener("connection", (socket) => {
-  console.log("Users connected:", ++usersCount);
-
   socket.addListener("message", (message) => {
-    console.log(message);
     // Пересылаем сообщение другому клиенту
     wsServer.clients.forEach((client) => {
       if (client !== socket && client.readyState === WebSocket.OPEN) {
         client.send(message);
       }
     });
-  });
-
-  socket.addListener("close", () => {
-    console.log("Users connected:", --usersCount);
   });
 });
