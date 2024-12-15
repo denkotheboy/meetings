6863f3278add20fe1504012de3ec3a360292d808
Kondratev.Denis <kondratev_dv@tochka.com>
2024-12-15 15:31:31 +0500

    fix(client): поправил парсер

--- client/src/ws.ts
+++ client/src/ws.ts
@@ -1,14 +1,15 @@
 import { webSocket, WebSocketSubject } from "rxjs/webSocket";
 
-type TMessage = Parameters<WebSocket["send"]>[0];
-
 export class Ws {
-  private _socket: WebSocketSubject<TMessage> = webSocket(
-    `wss://${window.location.host}/ws/`,
-  );
+  private _socket: WebSocketSubject<unknown> = webSocket({
+    url: `wss://${window.location.host}/ws/`,
+    deserializer: (e) => {
+      return e.data;
+    },
+  });
   messages$ = this._socket.asObservable();
 
-  send(msg: TMessage) {
+  send(msg: any) {
     return this._socket.next(msg);
   }
 }
