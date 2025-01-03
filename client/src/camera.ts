ab4267bc9ba825e3a0c6ab5b64a6a9d6c760a88e
Kondratev.Denis <kondratev_dv@tochka.com>
2025-01-04 03:13:00 +0500

    feat(client): изменил настройки камеры и звука

--- client/src/camera.ts
+++ client/src/camera.ts
@@ -10,11 +10,17 @@ export class Camera {
       video: {
         width: { ideal: 1920 },
         height: { ideal: 1080 },
-        frameRate: { ideal: 30, max: 60 },
+        frameRate: { ideal: 60, max: 60 },
         aspectRatio: 16 / 9,
         facingMode: "user",
       },
-      audio: true,
+      audio: {
+        sampleRate: { ideal: 48_000 }, // Частота дискретизации (48 кГц)
+        channelCount: { ideal: 2 }, // Стерео
+        echoCancellation: true, // Устранение эха
+        noiseSuppression: true, // Подавление шума
+        autoGainControl: true, // Подавление шума
+      },
     });
 
     return this._stream;
