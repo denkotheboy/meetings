15021a9d85f3eb4d3a599839783d4a3fe42710c1
Kondratev.Denis <kondratev_dv@tochka.com>
2025-01-04 02:20:44 +0500

    feat(server): переписал ws

--- server-go/main.go
+++ server-go/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"context"
-	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -13,15 +12,9 @@ import (
 	"github.com/coder/websocket"
 )
 
-// Структура для сообщений
-type Message struct {
-	Type string `json:"type"` // Тип сообщения (offer, answer, candidate)
-	Data string `json:"data"` // Данные сообщения
-}
-
 // Клиенты
 var clients = make(map[*websocket.Conn]bool)
-var broadcast = make(chan Message)
+var broadcast = make(chan []byte)
 var mu sync.Mutex
 
 func main() {
@@ -33,7 +26,7 @@ func main() {
 	go handleMessages()
 
 	// Запуск HTTP-сервера
-	fmt.Println("Сигнальный сервер запущен на :%s", port)
+	fmt.Println("Сигнальный сервер запущен на :", port)
 	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
 
@@ -57,8 +50,7 @@ func handleWebSocket(w http.ResponseWriter, r *http.Request) {
 
 	// Чтение сообщений
 	for {
-		var msg Message
-		err := readJSON(conn, &msg)
+		_, data, err := conn.Read(context.Background())
 		if websocket.CloseStatus(err) == websocket.StatusNormalClosure {
 			log.Println("Клиент отключился:", r.RemoteAddr)
 			break
@@ -68,7 +60,7 @@ func handleWebSocket(w http.ResponseWriter, r *http.Request) {
 		}
 
 		// Отправка сообщения в канал
-		broadcast <- msg
+		broadcast <- data
 	}
 
 	// Удаление клиента
@@ -87,7 +79,7 @@ func handleMessages() {
 		for client := range clients {
 			ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 			defer cancel()
-			err := writeJSON(ctx, client, msg)
+			err := client.Write(ctx, websocket.MessageText, msg)
 			if err != nil {
 				log.Println("Ошибка отправки сообщения:", err)
 				client.Close(websocket.StatusInternalError, "Ошибка отправки")
@@ -96,20 +88,4 @@ func handleMessages() {
 		}
 		mu.Unlock()
 	}
-}
-
-func readJSON(conn *websocket.Conn, v interface{}) error {
-	_, data, err := conn.Read(context.Background())
-	if err != nil {
-		return err
-	}
-	return json.Unmarshal(data, v)
-}
-
-func writeJSON(ctx context.Context, conn *websocket.Conn, v interface{}) error {
-	data, err := json.Marshal(v)
-	if err != nil {
-		return err
-	}
-	return conn.Write(ctx, websocket.MessageText, data)
 }
